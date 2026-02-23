beffcbabd1ee4e9ec8ec591c9542d6c6831b0e60
lordbyaku <lordbyaku@gmail.com>
2026-02-23 16:46:48 +0700

    up9

--- app/src/main/java/com/project/laundryappui/MainActivity.java
+++ app/src/main/java/com/project/laundryappui/MainActivity.java
@@ -2,6 +2,7 @@ package com.project.laundryappui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Color;
